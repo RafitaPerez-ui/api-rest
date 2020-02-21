404481758ee84079feb59ca9ddcca5ad3926f090
RafitaPerez-ui <58310879+RafitaPerez-ui@users.noreply.github.com>
2020-02-21 11:06:55 -0600

    new entity para el match

---
+++ Api-REST/src/main/java/com/proyecto/api/entity/Balance.java
@@ -0,0 +1,32 @@
+
+public class Balance {
+String acount;
+String balance;
+String owner;
+String creacion;
+public String getAcount() {
+	return acount;
+}
+public void setAcount(String acount) {
+	this.acount = acount;
+}
+public String getBalance() {
+	return balance;
+}
+public void setBalance(String balance) {
+	this.balance = balance;
+}
+public String getOwner() {
+	return owner;
+}
+public void setOwner(String owner) {
+	this.owner = owner;
+}
+public String getCreacion() {
+	return creacion;
+}
+public void setCreacion(String creacion) {
+	this.creacion = creacion;
+}
+
+}
