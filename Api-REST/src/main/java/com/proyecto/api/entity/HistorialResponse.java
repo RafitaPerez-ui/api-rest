a3924485bff0b60d67ce462041a28e84ea6d4107
RafitaPerez-ui <58310879+RafitaPerez-ui@users.noreply.github.com>
2020-02-21 11:06:06 -0600

    new entity

---
+++ Api-REST/src/main/java/com/proyecto/api/entity/HistorialResponse.java
@@ -0,0 +1,12 @@
+public class HistorialResponse extends ResponseTO {
+Balance balance;
+
+public Balance getBalance() {
+	return balance;
+}
+
+public void setBalance(Balance balance) {
+	this.balance = balance;
+}
+
+}
